fe277a155829a01f5282f6d74ce120816fa1f400
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-11-14 20:09:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -34,7 +34,7 @@ class Dog
         INSERT INTO dogs (name, breed)
         VALUES (?, ?)
         SQL
-        DB[:conn].execute(sql, self.name, self.album)
+        DB[:conn].execute(sql, self.name, self.breed)
         @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
     end
   end
