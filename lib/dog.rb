b9db754db87f629977f18d26cfe2f3710c377cc8
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-11-14 20:09:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -28,14 +28,14 @@ class Dog
 
   def save
     if self.id
-   self.update
- else
-   sql = <<-SQL
-     INSERT INTO songs (name, album)
-     VALUES (?, ?)
-   SQL
-   DB[:conn].execute(sql, self.name, self.album)
-   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+      self.update
+    else
+      sql = <<-SQL
+        INSERT INTO songs (name, album)
+        VALUES (?, ?)
+        SQL
+        DB[:conn].execute(sql, self.name, self.album)
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
     end
   end
 
