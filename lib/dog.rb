e3ec233dfc1bd2b0d96dcf4250f89cbcbd85641a
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-11-14 20:55:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -54,7 +54,7 @@ class Dog
  def self.find_by_id(name:, breed:)
     sql = "SELECT * FROM dogs WHERE id = ?"
     result = DB[:conn].execute(sql, id)[0]
-    Dog.new(result[0], result[1], result[2])
+    Dog.new(id: result[0], result[1], result[2])
   end
 
   def self.find_by_name(name)
