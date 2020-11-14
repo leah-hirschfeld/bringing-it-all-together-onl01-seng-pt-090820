b1ef60411f56e69018b7424f0502a006131ff48c
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-11-14 21:01:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -70,15 +70,15 @@ class Dog
    end.first
  end
 
- def self.find_or_create_by(name:, album:)
-    song = DB[:conn].execute("SELECT * FROM songs WHERE name = ? AND album = ?", name, album)
-    if !song.empty?
-      song_data = song[0]
-      song = Song.new(song_data[0], song_data[1], song_data[2])
+ def self.find_or_create_by(name:, breed:)
+    song = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
+    if !dogs.empty?
+      dog_data = dog[0]
+      dog = Dog.new(dog_data[0], dog_data[1], dog_data[2])
     else
-      song = self.create(name: name, album: album)
+      dog = self.create(name: name, breed: breed)
     end
-    song
+    dog
   end
 
   def update(id, name, breed)
