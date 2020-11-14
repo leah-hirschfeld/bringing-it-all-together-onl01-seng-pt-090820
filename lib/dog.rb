cb8465f0af3449cb9c67f2826c85260a60b117f6
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-11-14 20:11:52 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -40,10 +40,10 @@ class Dog
     self
   end
 
-  def self.create(name, album)
-   song = Song.new(name, album)
-   song.save
-   song
+  def self.create(name, breed)
+   dog = Dog.new(name, breed)
+   dog.save
+   dog
  end
 
 end
