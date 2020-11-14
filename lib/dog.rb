a8a0c8c44b6ca8fdae0ba0cb6f6c486208b5388a
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-11-14 20:08:50 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -27,6 +27,16 @@ class Dog
   end
 
   def save
+    if self.id
+   self.update
+ else
+   sql = <<-SQL
+     INSERT INTO songs (name, album)
+     VALUES (?, ?)
+   SQL
+   DB[:conn].execute(sql, self.name, self.album)
+   @id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+    end
   end
 
 end
