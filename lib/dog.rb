1c61cc91c79ffa69ada2f7af1becee6e70e12ebf
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-11-14 20:43:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -47,13 +47,12 @@ class Dog
  end
 
  def self.new_from_db(row)
- new_song = self.new  # self.new is the same as running Song.new
- new_song.id = row[0]
- new_song.name =  row[1]
- new_song.length = row[2]
- new_song  # return the newly created instance
-end
-
+   new_song = self.new  # self.new is the same as running Song.new
+   new_song.id = row[0]
+   new_song.name =  row[1]
+   new_song.length = row[2]
+   new_song  # return the newly created instance
+ end
 
  def self.find_by_id(name:, breed:)
     sql = "SELECT * FROM dogs WHERE id = ?"
