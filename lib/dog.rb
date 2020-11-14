01254fb793f78f4707793bf33af8ba7e9f2c5dbb
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-11-14 20:59:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -76,7 +76,7 @@ class Dog
       dog_data = dog[0]
       dog = Dog.new(id: dog_data[0], name: dog_data[1], breed: dog_data[2])
     else
-      dog = self.create(name, breed)
+      dog = self.create(attributes)
     end
     dog
   end
