ed23d300e675446d461e38851dd5439870e20b5c
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-11-14 20:47:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -85,7 +85,7 @@ class Dog
   end
 
   def update
-   sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+   sql = "UPDATE dogs SET name = ?, breed = ?"
    DB[:conn].execute(sql, self.name, self.breed)
   end
 
