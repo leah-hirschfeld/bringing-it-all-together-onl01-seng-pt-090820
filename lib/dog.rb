bc53ec16e3c4a879193d9d99d5bdae00a56b301a
Leah Hirschfeld <leah.hirschfeld@gmail.com>
2020-11-14 20:43:24 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -48,10 +48,10 @@ class Dog
 
  def self.new_from_db(row)
    new_dog = self.new  # self.new is the same as running Song.new
-   new_song.id = row[0]
-   new_song.name =  row[1]
-   new_song.length = row[2]
-   new_song  # return the newly created instance
+   new_dog.id = row[0]
+   new_dog.name =  row[1]
+   new_dog.breed = row[2]
+   new_dog  # return the newly created instance
  end
 
  def self.find_by_id(name:, breed:)
